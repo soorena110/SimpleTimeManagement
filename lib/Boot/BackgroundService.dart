6c8cf2ac12b99ca73003c7093adfd4ced32dcaab
azarang <Asus@DESKTOP-LVPKFR3>
2019-09-11 02:44:17 +0430

    چک همیشگی هر تیک

--- lib/Boot/BackgroundService.dart
+++ lib/Boot/BackgroundService.dart
@@ -1,6 +1,5 @@
 import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:time_river/Database/Provider.dart';
 import 'package:time_river/Models/Task.dart';
 import 'package:time_river/Services/TaskService.dart';
@@ -8,27 +7,11 @@ import 'package:time_river/Services/TaskService.dart';
 import 'NotificationCenter.dart';
 
 class BackgroundService {
-  static bool isCriticalState;
+  static bool isCriticalState = false;
   static List<Task> tasks;
   static int day = DateTime
       .now()
       .day;
-
-  static getStatusFromSharedPrefrence() async {
-    if (isCriticalState == null) {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      isCriticalState = prefs.getBool('isCritical') ?? false;
-    }
-    return isCriticalState;
-  }
-
-  static setStatusFromSharedPrefrence(bool status) async {
-    if (status == isCriticalState) return;
-
-    isCriticalState = status;
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isCritical', true);
-  }
 
   static _refreshStatusInSharedPreference() async {
     if (tasks == null || day != DateTime
@@ -43,16 +26,17 @@ class BackgroundService {
     final criticalTasks = tasks.where((task) => task.getIsCritical()).toList();
     final status = criticalTasks.length > 0;
 
-    Fluttertoast.showToast(
-        msg: criticalTasks.length.toString().toString(),
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        fontSize: 5);
-    setStatusFromSharedPrefrence(status);
+    if (isCriticalState)
+      Fluttertoast.showToast(
+          msg: criticalTasks.length.toString().toString(),
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          fontSize: 5);
+    isCriticalState = status;
   }
 
   static _notifyIfItIsCriticalStatus() async {
-    if (await getStatusFromSharedPrefrence()) await NotificationCenter.blink();
+    if (isCriticalState) await NotificationCenter.blink();
   }
 
   static searchCriticalSituationInDatabase() async {
@@ -68,21 +52,9 @@ class BackgroundService {
     }
   }
 
-  static int ticks = 0;
-
   static checkCriticalSituationInTimer() async {
-    ticks++;
-
-    if (ticks % 6 == 1) await searchCriticalSituationInDatabase();
+    await searchCriticalSituationInDatabase();
     await _notifyIfItIsCriticalStatus();
-
-    Fluttertoast.showToast(
-        msg: ticks.toString() +
-            ' -> ' +
-            (await getStatusFromSharedPrefrence()).toString(),
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        fontSize: 5);
   }
 
   static start() async {
@@ -95,7 +67,7 @@ class BackgroundService {
   }
 
   static stop() async {
-    await setStatusFromSharedPrefrence(false);
+    isCriticalState = false;
     await AndroidAlarmManager.cancel(1000);
   }
 }
